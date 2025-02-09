dbe584fe7dc04da8cc890242beb8fa05b496f1ee
Andrey Kuznecov <viirtualkuz@yandex.ru>
2025-02-10 03:46:19 +0800

    moved to project folder

---
+++ utils/utils.py
@@ -0,0 +1,20 @@
+import json
+import pickle
+import argparse
+
+def load_json(path):
+    with open(path, 'r') as file:
+        return json.load(file)
+
+def load_pkl(path):
+    with open(path, 'rb') as file:
+        return pickle.load(file)
+    
+def save_pkl(obj, path):
+    with open(path, 'wb') as file:
+        pickle.dump(obj, file)
+
+def parse_args(desc):
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('--config_path', type=str, required=True, help='Input path to the configuration file')
+    return parser.parse_args()
