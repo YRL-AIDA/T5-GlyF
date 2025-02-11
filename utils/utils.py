e4719646f5b70694965ac06720da30004322cbc2
Andrey Kuznecov <viirtualkuz@yandex.ru>
2025-02-11 20:16:15 +0800

    added functions to calculate metrics and plot losses chart

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,9 @@
 import json
 import pickle
 import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from Levenshtein import ratio
 
 def load_json(path):
     with open(path, 'r') as file:
@@ -18,3 +21,32 @@ def parse_args(desc):
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('--config_path', type=str, required=True, help='Input path to the configuration file')
     return parser.parse_args()
+
+def calculate_accruracy(y_pred, y_true):
+    accuracy = 0
+    test_size = len(y_pred)
+    for i in range(test_size):
+        if y_pred[i] == y_true[i]:
+            accuracy += 1
+    return accuracy/test_size
+
+def calculate_levenshtein_ratio(y_pred, y_true):
+    l_ratio = []
+    test_size = len(y_pred)
+    for i in range(test_size):
+        l_ratio.append(ratio(y_true[i], y_pred[i]))
+    return np.mean(l_ratio)
+
+def plot_losses(train_loss, val_loss, save_chart_path='losses.png'):
+    c_iter = range(len(train_loss))
+
+    fig = plt.figure(figsize=(15, 7), dpi=80)
+    ax = fig.gca()
+    ax.set_xticks(c_iter)
+    plt.plot(c_iter, train_loss, color='orange', label='train_loss')
+    plt.plot(c_iter, val_loss, color='blue', label='val_loss')
+
+    plt.legend(loc='upper right')
+    plt.title('Losses')
+    plt.grid()
+    plt.savefig(save_chart_path)
